72f47b679482a893b07715b5b3779bce943986b8
Nakul Bharti <knakul853@gmail.com>
2019-08-18 17:09:26 +0530

    Create Contains_Duplicate_III.cpp

---
+++ Trees/BST/Contains_Duplicate_III.cpp
@@ -0,0 +1,32 @@
+ bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) 
+    {
+        
+      if( nums.size()<2 || k==0)return false; 
+        
+        deque<int>dq;
+        multiset<long>mst;
+        int n = (long)nums.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            if(mst.size() > k)
+            {
+                mst.erase(mst.find(dq.front()));
+                dq.pop_front();
+            }
+            
+        auto it = mst.lower_bound(long(nums[i] - long(t)));
+            if(it==mst.end() || *it > (long)t + nums[i])
+            {
+                mst.insert(nums[i]);
+                dq.push_back(nums[i]);
+                
+            }
+            else
+                return true;
+        }
+        
+        return false;
+       
+        
+    }

c2b6483c84dfb3b48246dbadf0384b632b9934a4
Nakul Bharti <knakul853@gmail.com>
2019-08-26 23:38:33 +0530

    Update 300. Longest Increasing Subsequence.cpp

--- DP/300. Longest Increasing Subsequence.cpp
+++ DP/300. Longest Increasing Subsequence.cpp
@@ -25,4 +25,36 @@ public:
     }
 };
 
-// o(nlogn) solution
+// simple o(nlogn) solution :/
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0)return 0;
+        vector<int>ans;
+       for(int i=0;i<n;i++)
+       {
+           auto it = std::lower_bound(ans.begin(),ans.end(),nums[i]);
+           if(it == ans.end())ans.push_back(nums[i]);
+           else *it = nums[i];
+       }
+        return ans.size();
+        
+        
+        
+    }
+};
+// another style of LIS
+vector<int>f(n+1,0),dp(n+1,0);
+   int ans=0;
+    for(int i=1;i<=n;i++)
+    {
+        dp[i] = lower_bound(f.begin()+1,f.begin()+1+ans,v[i]) - f.begin();
+        ans = max(ans,dp[i]);
+        f[dp[i]] = v[i];
+    }
+ 
+    cout<<ans<<"\n"
+
+

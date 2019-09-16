16a200d4ba90601f7b491ddad6e1fcbe14911750
Nakul Bharti <knakul853@gmail.com>
2019-09-16 23:50:04 +0530

    Update Sliding_window.cpp

--- Sliding_window.cpp
+++ Sliding_window.cpp
@@ -1,5 +1,14 @@
-/// :  https://leetcode.com/problems/diet-plan-performance
+/// : https://leetcode.com/problems/diet-plan-performance
 /// : https://pastebin.com/i1hxx9Xv
 
 /// : http://leetcode.liangjiateng.cn/leetcode/find-k-length-substrings-with-no-repeated-characters/description
 /// : https://pastebin.com/zixWSiwc      ( sexy question)
+
+/// : http://leetcode.liangjiateng.cn/leetcode/minimum-swaps-to-group-all-1s-together/description
+/// : https://pastebin.com/yn6uge6i
+
+/// : https://leetcode.com/problems/max-consecutive-ones-iii/
+/// : https://pastebin.com/iM0mZKJR  ( SOlution idea from the discuss of leetcode)
+
+/// : https://leetcode.com/problems/grumpy-bookstore-owner/submissions/
+/// : https://pastebin.com/fVtwus73
